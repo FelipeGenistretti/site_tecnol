856451851e7d7b12592fce3d024853bd47c08ae7
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-10-29 13:13:02 -0300

    feat desenvolvendo formulario de pre-cadastro

--- routes/web.php
+++ routes/web.php
@@ -9,53 +9,37 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// Rotas de teste e home
-Route::get("/teste", function() {
-    return view("teste");
+// Rota de teste
+Route::get('/teste', function() {
+    return view('teste');
 });
 
-Route::get("/home", function() {
-    return view("home");
-})->name("home");
+// Formulário de fale-conosco
+Route::post('/fale-conosco', [FormController::class, 'store'])
+    ->name('fale-conosco.store');
 
+// Formulário de pré-cadastro (exibe o formulário)
+Route::get('/pre-registro', function() {
+    return view('pre-cadastro');
+})->name('pre-registro.create');
 
-
-<<<<<<< HEAD
-Route::get("/quem-somos", function(){
-    return view("quemsomos");
-=======
-// Formulários de envio
-Route::post("/fale-conosco", [FormController::class, 'store'])
-    ->name("fale-conosco.store");
-
-
-
-Route::get("/pre-registro", function() {
-    // Exibe o formulário de pré-cadastro
-    return view("pre-cadastro");
-})->name("pre-registro.create");
-
-Route::post("/pre-registro", [PreRegistroController::class, 'store'])
-    ->name("pre-registro.store");
-
-
-
-
+// Formulário de pré-cadastro (envio)
+Route::post('/pre-registro', [PreRegistroController::class, 'store'])
+    ->name('pre-registro.store');
 
 // Páginas institucionais
-Route::get("/quem-somos", function() {
-    return view("quem-somos");
->>>>>>> FDEV-28/10/2025
-})->name("quem-somos");
+Route::get('/quem-somos', function() {
+    return view('quemsomos');
+})->name('quem-somos');
 
-Route::get("/veiculo", function() {
-    return view("safe-register-car");
-})->name("safe-register-car");
+Route::get('/veiculo', function() {
+    return view('safe-register-car');
+})->name('safe-register-car');
 
-Route::get("/compliance", function() {
-    return view("compliance");
-})->name("compliance");
+Route::get('/compliance', function() {
+    return view('compliance');
+})->name('compliance');
 
-Route::get("/seguranca", function() {
-    return view("seguranca");
-})->name("seguranca");
+Route::get('/seguranca', function() {
+    return view('seguranca');
+})->name('seguranca');
