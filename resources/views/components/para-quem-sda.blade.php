afa05188e920f405cdc7cdc4c291672b618af1fe
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-11-12 10:21:29 -0300

    feat ajustando imagens na pagina do sda

--- resources/views/components/para-quem-sda.blade.php
+++ resources/views/components/para-quem-sda.blade.php
@@ -2,18 +2,19 @@
 
     <div class="bg-bgSecondary bg-contrast container-x gap-4 py-10 flex">
         <div>
-            <img src="/imgHeroQualidade.png" alt="">
+            <img src="/analiseDocumentoSda.png" width="836" height="466" alt="">
+
         </div>
 
         <div class="flex flex-col justify-center">
                 <h1 class="text-4xl text-textPrimary pb-4 text-contrast">Para instituições financeiras</h1>
-                <p class="text-textSecondary mb-4 text-contrast">O SDA é um aliado estratégico para reduzir riscos e aumentar a rentabilidade:</p>
-                <p><span class="font-bold text-sm">Decisões de crédito mais assertivas,</span> baseadas em dados estruturados e análises inteligentes.</p>
+                <p class="text-textSecondary mb-4 text-contrast text-sm">O SDA é um aliado estratégico para reduzir riscos e aumentar a rentabilidade:</p>
                 <ul class="list-disc list-inside text-sm">
+                    <li><span class="font-bold">Decisões de crédito mais assertivas,</span> baseadas em dados estruturados e análises inteligentes.</li>
                     <li><span class="font-bold">Mitigação de fraudes,</span> com mecanismos de validação e cruzamento de informações em tempo real.</li>
                     <li><span class="font-bold">Governança e compliance garantidos,</span> assegurando aderência às normas e rastreabilidade completa.</li>
+                    <li><span class="font-bold">Otimização de custos operacionais,</span> eliminando processos manuais e reduzindo retrabalho.</li>
                 </ul>
-                <p class="text-sm mt-1">Otimização de custos operacionais, eliminando processos manuais e reduzindo retrabalho.</p>
         </div>
     </div>
 
@@ -22,8 +23,8 @@
         <div class="flex flex-col justify-center">
             <div class="space-y-5">
                 <h1 class="text-4xl text-textPrimary text-contrast">Para Concessionárias e Revendas</h1>
-                <p class="text-textSecondary text-lg text-contrast">O SDA transforma a experiência comercial:</p>
-                <ul class="list-disc list-inside">
+                <p class="text-textSecondary text-md text-contrast">O SDA transforma a experiência comercial:</p>
+                <ul class="list-disc list-inside text-sm">
                     <li><span class="font-bold">Agilidade na aprovação de crédito,</span> reduzindo o tempo entre proposta e fechamento.</li>
                     <li><span class="font-bold">Integração com instituições financeiras,</span> evitando falhas de comunicação e acelerando processos.</li>
                     <li><span class="font-bold">Maior taxa de conversão,</span> pois clientes têm respostas rápidas e seguras, aumentando a satisfação e fidelização.</li>
@@ -31,7 +32,8 @@
             </div>
         </div> 
         <div>
-            <img src="/imgHeroQualidade.png" alt="">
+            <img src="/revendaImgSda.png" width="836" height="466" alt="">
+
         </div>
     </div>
 </div>
